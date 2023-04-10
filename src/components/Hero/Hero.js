bf846d185e85efa9bf70de2440576cf4148817ad
Anthony Quispe <71742544+AnthonyQuispe@users.noreply.github.com>
2023-04-09 23:37:56 -0400

    Desktop Styling almost complete

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,7 @@
+import React, { useState, useEffect } from "react";
 import Video from "../../data/video-details.json";
 import "./Hero.scss";
 import Details from "../HeroDetails/HeroDetails";
-import React, { useState, useEffect } from "react";
 import CommentList from "../Comments/Comments";
 
 function Videos() {
@@ -30,50 +30,55 @@ function Videos() {
   return (
     <div className="hero">
       {currentVideo && (
-        <div className="hero__video--container">
+        <div className="hero__video-container">
           <video
             className="hero__video"
             src={currentVideo.videoUrl}
             poster={currentVideo.image}
             controls
           />
+          <div className="video-container">
+            <div className="video-detail">
+              <Details selectedVideo={currentVideo} />
+              <CommentList
+                videoId={currentVideo?.id}
+                comments={Video[currentVideo?.id]?.comments || []}
+              />
+            </div>
+            <div className="next-video">
+              <h4 className="next-video__heading subheader">NEXT VIDEOS</h4>
+              <ul className="next-video__list">
+                {sideVideos.map((video) => (
+                  <li key={video.id} className="next-video__item">
+                    <div
+                      className="next-video__item-container"
+                      onClick={() => handleVideoSelect(video)}
+                    >
+                      <div className="next-video__image-container">
+                        <img
+                          className="next-video__image"
+                          src={video.image}
+                          alt={video.title}
+                          poster={video.image}
+                        />
+                      </div>
 
-          <Details selectedVideo={currentVideo} />
+                      <div className="next-video__content">
+                        <h3 className="next-video__title subheader">
+                          {video.title}
+                        </h3>
+                        <p className="next-video__channel body-copy">
+                          {video.channel}
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       )}
-      <CommentList
-        videoId={currentVideo?.id}
-        comments={Video[currentVideo?.id]?.comments || []}
-      />
-      <div className="next-video">
-        <h4 className="next-video__heading subheader">NEXT VIDEOS</h4>
-        <ul className="next-video__list">
-          {sideVideos.map((video) => (
-            <li key={video.id} className="next-video__item">
-              <div
-                className="next-video__item-container"
-                onClick={() => handleVideoSelect(video)}
-              >
-                <div className="next-video__image-container">
-                  <img
-                    className="next-video__image"
-                    src={video.image}
-                    alt={video.title}
-                    poster={video.image}
-                  />{" "}
-                </div>
-
-                <div className="next-video__content">
-                  <h3 className="next-video__title subheader">{video.title}</h3>
-                  <p className="next-video__channel body-copy">
-                    {video.channel}
-                  </p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 }
